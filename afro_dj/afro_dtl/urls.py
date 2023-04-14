b57274de13268ff6ad10b1c4c48c0bdbbbfdd7f6
Leilah <leilahlubira@gmail.com>
2023-04-14 13:14:57 +0100

    Finished the authentication process.

--- afro_dj/afro_dtl/urls.py
+++ afro_dj/afro_dtl/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('index', views.index, name='index'),
     path('register', views.register, name='register'),
     path('registration', views.registration, name='registration'),
+    path('login_user', views.login_user, name = 'login_user'),
+    path('login_page', views.login_page, name = 'login_page')
+    
+    
 ]
--- afro_dj/afro_dtl/views.py
+++ afro_dj/afro_dtl/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from .models import User_account
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
@@ -13,19 +15,45 @@ def index(request):
     )
 
 def register(request):
-
     return render(request, 'register.html')
 
 def registration(request):
     username = request.POST['username']
-    email = request.POST['email']
+    email = request.POST['user_email']
     password = request.POST['password']
     gender = request.POST['gender']
     user_details=[
-            username,email,password,gender
-        ]
+            username,email,password,gender]
+    
     print(user_details)
-    return render(request, 'index.html', {'username': username})
-   
+    if User.objects.filter(username=username).first():
+        print('Username already exists')
+        return render(request, 'login.html')
+    else:
+        user = User.objects.create_user(username, email, password)
+        return render(request, 'login.html')
 
+def login_user(request):
+    username = request.POST['username']
+    pwd = request.POST['password']
+    if User.objects.filter(username=username):
+        print('This username exists.')
+        logged_user = authenticate(request, username=username, password=pwd)
+        if logged_user is not None:
+            #Here we are logging in the user
+            auth_login(request, logged_user)
+            print(username + ' '+ 'Logged in successfully.')
+            return redirect('index')
+        else:
+            #handling a scenario when authentication has failed.
+            return render(request, 'login.html')
+    else:
+        print('User credentials do not exist.')
+        return render(request, 'login.html')
+    
+def login_page(request):
+    return render('login.html')
 
+    
+    
+    
